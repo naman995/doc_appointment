ca32dcb122c268d90e768f12c3df495e29043ae7
naman995 <rajputnaman823@gmail.com>
2024-03-04 00:52:28 +0530

    Basic setup

---
+++ src/app/pages/new-appointment/new-appointment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-appointment',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-appointment.component.html',
+  styleUrl: './new-appointment.component.css'
+})
+export class NewAppointmentComponent {
+
+}
